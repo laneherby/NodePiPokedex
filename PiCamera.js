6e069c88b1b1a89d62c20da2b14b39acf38486fc
laneherby <laneherberholz@gmail.com>
2020-08-09 07:25:31 +0100

    Merged PiCamera with existing app

---
+++ PiCamera.js
@@ -0,0 +1,21 @@
+const PiCamera = require("pi-camera");
+
+const takePicture = async () => {
+    const myCamera = new PiCamera({
+        mode: "photo",
+        output: `${__dirname}/pokeScan.jpg`,
+        width: 640,
+        height: 480,
+        nopreview: true,
+    });
+
+    try {
+        const result = await myCamera.snap();
+        return true;
+    } catch (e) {
+        console.log(e);
+        return "Error while taking picture";
+    }
+}
+
+exports.takePicture = takePicture();
--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const fs = require("fs");
 const imgurUploader = require("imgur-uploader");
 const googleSearchByImageURL = require("./googleSearchByImageURL");
+const piCamera = require("./PiCamera");
 const say = require('say');
 
 const main = async () => {
-    // const imgurData = await imgurUploader(fs.readFileSync("filelocation"), {title: "PokeDex"});
+    const pictureResult = await piCamera.takePicture();
 
-    //const pokemonScanned = await googleSearchByImageURL.imageSearch("imgurData.link");  
+    if (pictureResult === true) {
+        try {
+            const imgurData = await imgurUploader(fs.readFileSync("./pokeScan.jpg"), {title: "PokeDex"});
+            const pokemonScanned = await googleSearchByImageURL.imageSearch(imgurData.link); 
 
-    if (pokemonScanned === false) {
-        say.speak("Unable to find a powkeimaan");
+            if (pokemonScanned === false) {
+                say.speak("Unable to find a powkeimaan");
+            } else {
+                say.speak(pokemonScanned);
+            }
+        } catch (e) {
+            console.log(e);
+            say.speak("Error while scanning. Check internet connection.");
+        }
     } else {
-        say.speak(pokemonScanned);
+        say.speak(pictureResult);
     }
 };
 
